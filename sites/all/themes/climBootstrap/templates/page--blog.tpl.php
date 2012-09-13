6dfc1ca21c4759c45ed40c26f69024682bd283e0
fbushell <frank@gocirro.com>
2012-09-13 17:33:06 -0600

    fixed the blog pinterest

--- sites/all/themes/climBootstrap/templates/page--blog.tpl.php
+++ sites/all/themes/climBootstrap/templates/page--blog.tpl.php
@@ -17,7 +17,7 @@
 <div class="container" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div id="content">
     <?php print $messages; ?>
-    <?php include('blog.inc'); ?>
+    <?php global $base_url; include('blog.inc'); ?>
     <!--<div id="scroller"></div>-->
   </div> 	
 </div> 
